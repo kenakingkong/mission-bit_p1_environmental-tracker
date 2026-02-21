06bbdef82abfbfcbafb795025858d0d430fdb50c
Makena Kong <kena.kong@gmail.com>
2026-02-20 23:40:43 -0800

    project 1 complete

--- script_p1.py
+++ script_p1.py
@@ -8,9 +8,19 @@ Part 1
 - Test code.
 """
 
-def main():
-  # write code here
-  return
+WATER_USED_PER_SHOWER = 2  # avg 2g water used per 1min shower
 
-if __name__ == '__main__':
+def main():
+    total_weekly_showers = input("How many showers do you take per week? ")
+    avg_shower_length = input("How long are your showers (in minutes)? ")
+    weekly_water_usage = int(total_weekly_showers) * (int(avg_shower_length) * WATER_USED_PER_SHOWER)
+
+    # print("\nOn average, you take " + avg_shower_length + " minute long showers, " + total_weekly_showers + " times per week\n")
+    print("\nYou shower {} times per week and take {} minute long showers".format(total_weekly_showers, avg_shower_length))
+    print("This means you use {} gallons of water a week\n".format(weekly_water_usage))
+
+    return
+
+
+if __name__ == "__main__":
     main()
