bcc568037248093eda393ef9419fd1830a26e4e3
Makena Kong <kena.kong@gmail.com>
2026-02-20 23:46:48 -0800

    attempted project 2 lol

--- script_p2.py
+++ script_p2.py
@@ -7,10 +7,33 @@ Part 2
 - Test code.
 """
 
+# todo, missing the meals environmental prompts
+
+TOTAL_WEEKLY_SHOWERS_PROMPT = "How many showers do you take per week? "
+AVG_SHOWER_LENGTH_PROMPT = "How long are your showers (in minutes)? "
+TOTAL_DAILY_MEALS_PROMPT = "How many meals do you eat in a day? "
+
+WATER_USED_PER_SHOWER = 2  # avg 2g water used per 1min shower
 
 def main():
-  # write code here
-  return
+    total_weekly_showers = input(TOTAL_WEEKLY_SHOWERS_PROMPT)
+    avg_shower_length = input(AVG_SHOWER_LENGTH_PROMPT)
 
-if __name__ == '__main__':
+    weekly_water_usage = int(total_weekly_showers) * (int(avg_shower_length) * WATER_USED_PER_SHOWER)
+
+    print("\nYou shower {} times per week and take {} minute long showers".format(total_weekly_showers, avg_shower_length))
+    print("This means you use {} gallons of water in a week\n".format(weekly_water_usage))
+
+
+    total_daily_meals = input(TOTAL_DAILY_MEALS_PROMPT)
+
+    # do calculation here???
+
+    print("You eat {} meals in a day".format(total_daily_meals))
+    print("This means you use ___ of ___ in a week".format(total_daily_meals))
+
+    return
+
+
+if __name__ == "__main__":
     main()
